c1da4e339750dfb45b1421f1f5d71906031ab6e8
QuocThai1433 <101701785+QuocThai1433@users.noreply.github.com>
2022-10-25 22:26:42 +0700

    moviewebsite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Search from 'antd/lib/input/Search';
+import './App.scss';
+import Filter from './components/filter';
+import Navbar from './components/navbar';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='header'>
+        <div className='title'>
+          <label>PhimHay.Net</label>
+          <Search className='search' placeholder="Tìm:tên phim, đạo diễn, diễn viên" enterButton />
+        </div>
+        <Navbar />
+        <Filter />
+      </div>
+      <div className='body'>
+        <div className='left'></div>
+        <div className='right'></div>
+      </div>
+      <div className='footer'>
+
+      </div>
     </div>
   );
 }
 
 export default App;
+
+  // function getUser() {
+  //   var token = localStorage.getItem("token");
+  //   return axios.get(`${baseUrl}/user/1`,
+  //     {
+  //       headers: {
+  //         "Authorization": `Bearer ${token}`
+  //       }
+  //     }
+  //   ).then((response) => {
+
+  //   })
+  // }
+
+
